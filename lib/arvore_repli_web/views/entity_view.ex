7152f162facf1aef3e0eb51f1e71ead78103fb23
unknown <az.carta@gmail.com>
2022-01-19 13:44:48 -0300

    Heroku taking too much time to set up, used AWS E2 to create a vm to maintain the api running (working properly), fixed render_many not rendering all data from entities table. TODO: write some tests

--- lib/arvore_repli_web/views/entity_view.ex
+++ lib/arvore_repli_web/views/entity_view.ex
@@ -3,7 +3,7 @@ defmodule ArvoreRepliWeb.EntityView do
   alias ArvoreRepliWeb.EntityView
 
   def render("index.json", %{entities: entities}) do
-    %{data: render_many(entities, EntityView, "entity.json")}
+    %{data: render_many(entities, EntityView, "allentites.json")}
   end
 
   def render("show.json", %{entity: entity}) do
@@ -12,13 +12,24 @@ defmodule ArvoreRepliWeb.EntityView do
 
   def render("entity.json", %{entity: entity, subtree: subtree}) do
     %{
+      data: %{
         id: entity.id,
         name: entity.name,
         entity_type: entity.entity_type,
         inep: entity.inep,
         parent_id: entity.parent_id,
         subtree: subtree
+      }
+    }
+  end
 
+  def render("allentites.json", %{entity: entities}) do
+    %{
+      id: entities.id,
+      name: entities.name,
+      entity_type: entities.entity_type,
+      inep: entities.inep,
+      parent_id: entities.parent_id
     }
   end
 end
