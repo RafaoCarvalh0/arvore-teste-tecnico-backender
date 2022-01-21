038cb09bd242ca1f7e52b14e1670e3b6cde7f687
unknown <az.carta@gmail.com>
2022-01-21 12:27:41 -0300

    Changed if to cond do. Added a condition to validate if an entity_type:class has a parent_id, if not, response will be Bad Request (400)

--- lib/arvore_repli_web/controllers/entity_controller.ex
+++ lib/arvore_repli_web/controllers/entity_controller.ex
@@ -14,18 +14,21 @@ defmodule ArvoreRepliWeb.EntityController do
   def create(conn, %{"entity" => entity_params}) do
     type = entity_params["entity_type"]
 
-    if type == "network" or type == "school" or type == "class" do
-      with {:ok, %Entity{} = entity} <- Entities.create_entity(entity_params) do
-        conn
-        |> put_status(:created)
-        |> put_resp_header("location", Routes.entity_path(conn, :show, entity))
-        |> render("entity.json",
-          entity: entity,
-          subtree: Entities.get_subtree!(entity, entity.entity_type)
-        )
-      end
-    else
-      {:error, :"400"}
+    cond do
+      type == "network" or type == "school" or
+          (type == "class" and entity_params["parent_id"] != nil) ->
+        with {:ok, %Entity{} = entity} <- Entities.create_entity(entity_params) do
+          conn
+          |> put_status(:created)
+          |> put_resp_header("location", Routes.entity_path(conn, :show, entity))
+          |> render("entity.json",
+            entity: entity,
+            subtree: Entities.get_subtree!(entity, entity.entity_type)
+          )
+        end
+
+      true ->
+        {:error, :"400"}
     end
   end
 
@@ -39,15 +42,18 @@ defmodule ArvoreRepliWeb.EntityController do
     entity = Entities.get_entity!(id)
     type = entity_params["entity_type"]
 
-    if type == "network" or type == "school" or type == "class" do
-      with {:ok, %Entity{} = entity} <- Entities.update_entity(entity, entity_params) do
-        render(conn, "entity.json",
-          entity: entity,
-          subtree: Entities.get_subtree!(entity, entity.entity_type)
-        )
-      end
-    else
-      {:error, :"400"}
+    cond do
+      type == "network" or type == "school" or
+          (type == "class" and entity_params["parent_id"] != nil) ->
+        with {:ok, %Entity{} = entity} <- Entities.update_entity(entity, entity_params) do
+          render(conn, "entity.json",
+            entity: entity,
+            subtree: Entities.get_subtree!(entity, entity.entity_type)
+          )
+        end
+
+      true ->
+        {:error, :"400"}
     end
   end
 
